a2db83ef4a62cc695ebb22c00a3c2c4faf0100b0
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2023-08-24 11:10:46 +0200

    Add User routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import productRoutes from "./routes/productRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import cors from "cors";
 const port = process.env.PORT || 5000;
 
@@ -24,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
