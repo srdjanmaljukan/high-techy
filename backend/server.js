24587235724cf9ee855288183a2cc1d3f4ae8e35
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2023-08-31 15:10:28 +0200

    Set static folder for production

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,6 @@ app.use(cookieParser());
 
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-    res.send("API si running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -44,6 +40,18 @@ app.get("/api/config/paypal", (req, res) => res.send({ clientId: process.env.PAY
 
 const __dirname = path.resolve(); //Set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    //Any route that is not api will be redirected to index.html
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")))
+} else {
+    app.get("/", (req, res) => {
+        res.send("API si running...");
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
